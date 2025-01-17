d95aafeadb73991a0b379c5d58a27c67d0fe3626
MaratFront <alievmarat3988@gmail.com>
2025-01-17 04:06:27 +0200

    memory

--- src/pages/components/MemoryTimer/MemoryTimer.tsx
+++ src/pages/components/MemoryTimer/MemoryTimer.tsx
@@ -5,13 +5,18 @@ import { useTypedSelector } from "../../../customHooks/TypedUseSelector";
 import { tick } from "../../../Store/Slices/timer";
 export default function MemoryTimer() {
   const { minutes, seconds } = useTypedSelector((state) => state.timer);
+  const { modalType, modalFlag } = useTypedSelector((state) => state.modalFlag);
   const dispatch = useDispatch();
   useEffect(() => {
     const interval = setInterval(() => {
       dispatch(tick());
     }, 1000);
+    console.log(modalType);
+    if (modalFlag === true && modalType === "onePlayerWinner") {
+      clearInterval(interval);
+    }
     return () => clearInterval(interval);
-  }, [dispatch]);
+  }, [dispatch, modalFlag]);
   return (
     <div className="timer">
       <p className="timer__title">Time</p>
