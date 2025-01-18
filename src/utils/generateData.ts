cd8aad73ad2735c0381a68b10e7ddfdb8921bc64
MaratFront <alievmarat3988@gmail.com>
2025-01-18 11:22:37 +0200

    memory

--- src/utils/generateData.ts
+++ src/utils/generateData.ts
@@ -1,12 +1,12 @@
 function randomArr4x4() {
   const arr = [1, 2, 3, 4, 5, 6, 7, 8];
-  const resultArr4x4 = arr.sort(() => Math.random() - 0.5);
-  return [...resultArr4x4, ...resultArr4x4];
+  const resultArr4x4 = arr;
+  return [...resultArr4x4, ...resultArr4x4].sort(() => Math.random() - 0.5);
 }
 function randomArr6x6() {
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
-  const resultArr6x6 = arr.sort(() => Math.random() - 0.5);
-  return [...resultArr6x6, ...resultArr6x6];
+  const resultArr6x6 = arr;
+  return [...resultArr6x6, ...resultArr6x6].sort(() => Math.random() - 0.5);
 }
 
 function randomArrEmoji6x6() {
@@ -30,12 +30,12 @@ function randomArrEmoji6x6() {
     "😭",
     "🦄",
   ];
-  const resultArr6x6 = emojiArr.sort(() => Math.random() - 0.5);
-  return [...resultArr6x6, ...resultArr6x6];
+  const resultArr6x6 = emojiArr;
+  return [...resultArr6x6, ...resultArr6x6].sort(() => Math.random() - 0.5);
 }
 function randomArrEmoji4x4() {
   const emojiArr = ["🐶", "😢", "🥳", "🤔", "🍔", "😊", "🐔", "😍"];
-  const resultArr4x4 = emojiArr.sort(() => Math.random() - 0.5);
-  return [...resultArr4x4, ...resultArr4x4];
+  const resultArr4x4 = emojiArr;
+  return [...resultArr4x4, ...resultArr4x4].sort(() => Math.random() - 0.5);
 }
 export { randomArr4x4, randomArr6x6, randomArrEmoji4x4, randomArrEmoji6x6 };
